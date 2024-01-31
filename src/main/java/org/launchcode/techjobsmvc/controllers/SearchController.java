e368db5b685bc93f80ffe79646a92e49d50d2345
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-01-31 14:07:33 -0600

    all steps done, one fail :D

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,16 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -13,7 +20,7 @@ import static org.launchcode.techjobsmvc.controllers.ListController.columnChoice
  */
 @Controller
 @RequestMapping("search")
-public class SearchController {
+public class SearchController extends TechJobsController {
 
     @GetMapping(value = "")
     public String search(Model model) {
@@ -21,7 +28,36 @@ public class SearchController {
         return "search";
     }
 
+    @PostMapping(value = "")
+    public String displaySearch(Model model,
+                                @RequestParam String searchType,
+                                @RequestParam String searchTerm) {
+        ArrayList<Job> jobs = new ArrayList<Job>();
+        if (Objects.equals(searchTerm, "all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("searchType", searchType);
+        return "search";
+    }
+
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model,
+                                       @RequestParam String searchType,
+                                       @RequestParam String searchTerm) {
+        ArrayList<Job> jobs = new ArrayList<Job>();
+        if (Objects.equals(searchTerm, "all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("searchType", searchType);
+        return "search";
+    }
 }
-
---
+++ src/main/java/org/launchcode/techjobsmvc/controllers/TechJobsController.java
@@ -0,0 +1,18 @@
+package org.launchcode.techjobsmvc.controllers;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+
+public class TechJobsController {
+    static HashMap<String, String> actionChoices = new HashMap<>();
+
+    public void actionChoices() {
+        actionChoices.put("search", "Search");
+        actionChoices.put("list", "List");
+    }
+
+    public static HashMap <String, String> getActionChoices (@ModelAttribute HashMap<String, String> actionChoices){
+        return actionChoices;
+    }
+}
