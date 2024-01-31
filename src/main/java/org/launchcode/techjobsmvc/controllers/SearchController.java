39c70fadd0814b7835cc871be4429d5ab93f7859
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-01-31 14:22:14 -0600

    FORGOT WORKFLOW

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -29,9 +29,7 @@ public class SearchController extends TechJobsController {
     }
 
     @PostMapping(value = "")
-    public String displaySearch(Model model,
-                                @RequestParam String searchType,
-                                @RequestParam String searchTerm) {
+    public String displaySearch(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs = new ArrayList<Job>();
         if (Objects.equals(searchTerm, "all") || searchTerm.isEmpty()) {
             jobs = JobData.findAll();
@@ -46,9 +44,7 @@ public class SearchController extends TechJobsController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping(value = "results")
-    public String displaySearchResults(Model model,
-                                       @RequestParam String searchType,
-                                       @RequestParam String searchTerm) {
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs = new ArrayList<Job>();
         if (Objects.equals(searchTerm, "all") || searchTerm.isEmpty()) {
             jobs = JobData.findAll();
